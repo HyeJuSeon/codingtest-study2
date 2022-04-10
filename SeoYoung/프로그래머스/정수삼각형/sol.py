34bc65c3f43c3dd0eab30da53c64f4807dd32528
bsy1141 <bsy1141@naver.com>
2022-04-10 14:39:35 +0900

    4/10 study

---
+++ SeoYoung/프로그래머스/정수삼각형/sol.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    n=len(triangle)
+    
+    dp=[[0]*(i+1) for i in range(n+1) ]
+    dp[0][0]=triangle[0][0]
+    
+    for i in range(1,n):
+        for j in range(len(triangle[i])):
+            if j==0:
+                dp[i][j]=dp[i-1][j]+triangle[i][j]
+            elif j==len(triangle[i])-1:
+                dp[i][j]=dp[i-1][j-1]+triangle[i][j]
+            else:
+                dp[i][j]=max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j]
+    
+    return max(dp[n-1])
