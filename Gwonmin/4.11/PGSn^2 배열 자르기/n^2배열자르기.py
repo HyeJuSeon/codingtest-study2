ca5d49b8cdca8f0f37e60e18a62ef6dea3ec4a17
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-11 12:35:34 +0900

    좌표접근

--- Gwonmin/4.11/PGSn^2 배열 자르기/n^2배열자르기.py
+++ Gwonmin/4.11/PGSn^2 배열 자르기/n^2배열자르기.py
@@ -4,6 +4,20 @@
 # 1행, 2행, ..., n행을 잘라내어 모두 이어붙인 새로운 1차원 배열을 만듭니다.
 # 새로운 1차원 배열을 arr이라 할 때, arr[left], arr[left+1], ..., arr[right]만 남기고 나머지는 지웁니다.
 
+import math
+#좌표로 접근해야한다
+def solution(n, left, right):
+    answer = []
+
+    #left, right int()로 감싸줘야 런타임에러 안나는데 왜 그런지 모르겠다
+    for num in range(int(left),int(right)+1):
+        row = math.floor(num/n)
+        col = num % n
+        max_num = row+1 if row > col else col+1
+        answer.append(max_num)
+    return answer
+
+    
 #무지성 시간초과 2중 for문 없애야함
 '''
 def solution(n, left, right):
@@ -23,16 +37,4 @@ def solution(n, left, right):
 
     return answer
 '''
-import math
-#좌표로 접근해야한다
-def solution(n, left, right):
-    answer = []
-
-    #left, right int()로 감싸줘야 런타임에러 안나는데 왜 그런지 모르겠다
-    for num in range(int(left),int(right)+1):
-        row = math.floor(num/n)
-        col = num % n
-        max_num = row+1 if row > col else col+1
-        answer.append(max_num)
-    return answer
 
