cb71248db300f119e8f4f8242a8324abcf7ddf71
HyeJuSeon <hyejooseon@gmail.com>
2022-04-14 20:08:26 +0900

    연구소

--- Hyeju/14502.py
+++ Hyeju/14502.py
@@ -5,16 +5,16 @@ import copy
 
 N, M = map(int, sys.stdin.readline().split())
 Map = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-v = [(x, y) for x in range(N) for y in range(M) if Map[x][y] == 2]
+v = [(i, j) for i in range(N) for j in range(M) if Map[i][j] == 2]
 dq = deque(v)
-e = [(x, y) for x in range(N) for y in range(M) if Map[x][y] == 0]
+e = [(i, j) for i in range(N) for j in range(M) if Map[i][j] == 0]
 cases = list(combinations(e, 3))
 ans = []
 for case in cases:
     m = copy.deepcopy(Map)
     q = copy.deepcopy(dq)
-    for x, y in case:
-        m[x][y] = 1
+    for i, j in case:
+        m[i][j] = 1
     dx, dy = (1, -1, 0, 0), (0, 0, 1, -1)
     while q:
         a, b = q.popleft()
