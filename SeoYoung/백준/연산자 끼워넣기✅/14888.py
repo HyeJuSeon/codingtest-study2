0c72b36e0716d675b163348fece8bde4d1f35f4c
bsy1141 <bsy1141@naver.com>
2022-04-25 18:04:16 +0900

    설명 추가

--- SeoYoung/백준/연산자 끼워넣기✅/14888.py
+++ SeoYoung/백준/연산자 끼워넣기✅/14888.py
@@ -24,6 +24,7 @@ operands=list(map(int,input().split()))
 temp=list(map(int,input().split()))
 operators=[]
 
+# temp=[0, 0, 2, 1] => operators=[2,2,3] 이런 식으로 넣어줌
 for i in range(4):
     for j in range(temp[i]):
         operators.append(i)
@@ -31,9 +32,12 @@ for i in range(4):
 max_val=-math.inf
 min_val=math.inf
 
+# operators의 순열을 구함
 for permu in permutations(operators,n-1):
+    # 해당 operator 순열로 계산한 결과값 val
     val=cal(operands, permu)
 
+    # val을 현재 최댓값, 최솟값과 비교하여 갱신
     max_val=max(max_val, val)
     min_val=min(min_val, val)
 
