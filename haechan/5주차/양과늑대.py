958c80a73e25ebc17879582ffeea7575e0f05ef5
haechan <kiki9510@gmail.com>
2022-05-12 19:02:10 +0900

    수정

--- haechan/5주차/양과늑대.py
+++ haechan/5주차/양과늑대.py
@@ -1,20 +1,19 @@
 def dfs(cur, sheep, wolf, node, info, tree, s_cnt):
-    if info[cur] == 0:
-        sheep += 1
-    else:
-        wolf -= 1
+    if info[cur] == 0: sheep += 1
+    else: wolf -= 1
+
     if sheep + wolf > 0: # 양이 더 많은 경우
         if sheep > s_cnt[-1]: # 현재 최대 양의 수보다 많으면 추가
             s_cnt.append(sheep)
         node += tree[cur]
-        for i, n in enumerate(node) :
+        for i, n in enumerate(node): # 현재 노드인 n을 제외한 갈 수 있는 나머지 노드를 넣어준다
             dfs(n, sheep, wolf, node[:i]+node[i+1:], info, tree, s_cnt)
     else:
         return
 
 def solution(info, edges):
     tree = [[] for _ in range(len(info))]
-    for a, b in edges :
+    for a, b in edges:
         tree[a].append(b)
 
     s_cnt = [0]
