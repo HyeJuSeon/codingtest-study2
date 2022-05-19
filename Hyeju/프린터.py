6a7ed531b3d14160dd1c004df95eecc763f29faa
HyeJuSeon <hyejooseon@gmail.com>
2022-05-19 14:51:54 +0900

    lv2

--- Hyeju/프린터.py
+++ Hyeju/프린터.py
@@ -13,5 +13,5 @@ def solution(priorities, location):
         else:
             q.append(tmp)
 
-print(solution([2, 1, 3, 2], 2))
-print(solution([1, 1, 9, 1, 1, 1], 0))
+# print(solution([2, 1, 3, 2], 2))
+# print(solution([1, 1, 9, 1, 1, 1], 0))
